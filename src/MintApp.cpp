ed51c4210d795e259fb797d917385b43ab950fcd
Alexey Nabrodov <alexey.nabrodov@team.wrike.com>
2018-05-30 22:57:33 +0300

    eextract palette

--- src/MintApp.cpp
+++ src/MintApp.cpp
@@ -11,17 +11,6 @@ using namespace ci;
 using namespace ci::app;
 using namespace std;
 
-auto darkPalette = Palette{
-    "dark",
-    ci::Color(0xcc/255.f, 0xcc/255.f, 0xcc/255.f),
-    ci::Color(0x2e/255.f, 0x2e/255.f, 0x2e/255.f)};
-
-auto lightPalette = Palette{
-    "light",
-    ci::Color(0x11/255.f, 0x11/255.f, 0x11/255.f),
-    ci::Color(0xcc/255.f, 0xcc/255.f, 0xcc/255.f)};
-
-
 int HOffset = 24;
 int VOffset = 24;
 // std::string DEFAULT_FONT = "Iosevka 14";
@@ -46,7 +35,7 @@ void MintApp::setup() {
 	mPango->setMaxSize(getWindowWidth(), getWindowHeight());
 
     steps = std::make_shared<Steps>();
-    steps->state->currentPalette = darkPalette;
+    steps->state->currentPalette = palettes::DARK;
     steps->start();
 }
 
@@ -67,8 +56,8 @@ void MintApp::keyDown(KeyEvent event) {
 			mPango->setSpacing(mPango->getSpacing() - 1.0);
 			break;
 		case KeyEvent::KEY_t:
-            steps->state->currentPalette = steps->state->currentPalette.name == darkPalette.name ?
-                lightPalette : darkPalette;
+            steps->state->currentPalette = steps->state->currentPalette.name == palettes::DARK.name ?
+                palettes::LIGHT : palettes::DARK;
 			break;
 		case KeyEvent::KEY_i:
 			mPango->setDefaultTextItalicsEnabled(!mPango->getDefaultTextItalicsEnabled());
---
+++ src/palette.cpp
@@ -0,0 +1,57 @@
+#include "mint/palette.hpp"
+
+
+const Palette palettes::DARK = Palette{
+    "dark",
+    ci::Color(0xcc/255.f, 0xcc/255.f, 0xcc/255.f),
+    ci::Color(0x2e/255.f, 0x2e/255.f, 0x2e/255.f)};
+
+const Palette palettes::LIGHT = Palette{
+    "light",
+    ci::Color(0x11/255.f, 0x11/255.f, 0x11/255.f),
+    ci::Color(0xcc/255.f, 0xcc/255.f, 0xcc/255.f)};
+
+
+/*
+ // Dark
+    @black:           #1d1f21; // 00
+    @very-dark-gray:  #282a2e; // 01
+    @dark-gray:       #373b41; // 02
+    @gray:            #969896; // 03
+    @light-gray:      #b4b7b4; // 04
+    @very-light-gray: #c5c8c6; // 05
+    @almost-white:    #e0e0e0; // 06
+    @white:           #ffffff; // 07
+
+    // Colors
+    @red:             #cc6666; // 08
+    @orange:          #de935f; // 09
+    @yellow:          #f0c674; // 0A
+    @green:           #b5bd68; // 0B
+    @cyan:            #8abeb7; // 0C
+    @blue:            #81a2be; // 0D
+    @purple:          #b294bb; // 0E
+    @brown:           #a3685a; // 0F
+ */
+
+/*
+ // Light
+    @black:           #1d1f21; // 00
+    @very-dark-gray:  #282a2e; // 01
+    @dark-gray:       #373b41; // 02
+    @gray:            #969896; // 03
+    @light-gray:      #b4b7b4; // 04
+    @very-light-gray: #c5c8c6; // 05
+    @almost-white:    #e0e0e0; // 06
+    @white:           #ffffff; // 07
+
+    // Colors
+    @red:             #cc6666; // 08
+    @orange:          #de935f; // 09
+    @yellow:          #f0c674; // 0A
+    @green:           #b5bd68; // 0B
+    @cyan:            #8abeb7; // 0C
+    @blue:            #81a2be; // 0D
+    @purple:          #b294bb; // 0E
+    @brown:           #a3685a; // 0F
+ */
